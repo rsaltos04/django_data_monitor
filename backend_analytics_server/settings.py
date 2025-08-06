f85c6040b440224ece76bd5951b9c216ad9cefd8
rsaltos04 <150490675+rsaltos04@users.noreply.github.com>
2025-08-06 20:05:36 +0000

    guia 24 autenticacion

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -25,7 +25,16 @@ SECRET_KEY = "django-insecure-6j@1@q_*wj07!lynz_i0ds_lo%)ds*3d84xv$6dobcvpt21bme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = [
+  "https://*.app.github.dev", # Solo si utiliza Codespaces
+  "https://localhost:8000",
+  "http://127.0.0.1:8000"
+]
+
+
+ALLOWED_HOSTS = [
+    "*",
+    ]
 
 
 # Application definition
