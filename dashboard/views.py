bbeec0409d2ffac8d5a6572f79a33be2e52c11e8
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-08-12 17:54:37 +0000

    Para srr, todavia falta

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,12 +9,19 @@ from django.conf import settings
 def index(request):
     response = requests.get(settings.API_URL)  # URL de la API
     posts = response.json()  # Convertir la respuesta a JSON
-
     # Número total de respuestas
     total_responses = len(posts)
+    #tal vez hacer for al posts
+    #tal que por reserva en post se buscan los servicios
+    #por servicios en reserva se hacen 3 variables
+    # si existe el servicio en servicios, se le suma 1 a la variable, caso contrario, se siquie nomás
+    # de ahí, devolver eso en data
     data = {
         'title': "Landing Page' Dashboard",
         'total_responses': total_responses,
     }
 
+
     return render(request, 'dashboard/index.html', data)
+
+
