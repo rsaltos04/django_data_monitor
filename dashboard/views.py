10cf6691bbaf69048e59e69bcfec4ba8a79a7c6e
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-08-13 18:23:56 +0000

    guia 24 cambios

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
-
+@login_required
 def index(request):
     response = requests.get(settings.API_URL)  # URL de la API
     posts = response.json()  # Convertir la respuesta a JSON
